133eb3f96dd5d873a0b9506995953f1b37564312
paul <paul.tsogbe@gmail.com>
2024-08-09 14:25:26 +0200

    file modify

--- ProductService/app/routes/product.js
+++ ProductService/app/routes/product.js
@@ -13,3 +13,4 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
+
--- ProductService/server.js
+++ ProductService/server.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const productRoutes = require('./app/routes/productRoute');
-const { config } = require('dotenv');
-const app = express();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const productRoutes = require('./app/routes/productRoute');
+// const { config } = require('dotenv');
+// const app = express();
 
-require('dotenv').config();
+// require('dotenv').config();
 
-app.use(express.json());
+// app.use(express.json());
 
-console.log('MONGODB_URL:', process.env.MONGODB_URL);
+// console.log('MONGODB_URL:', process.env.MONGODB_URL);
 
-mongoose.connect(process.env.MONGODB_URL, {ssl: process.env.MONGO_SSL})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Could not connect to MongoDB', error));
-// Route de Product
-app.use('/', productRoutes);
+// mongoose.connect(process.env.MONGODB_URL, {ssl: process.env.MONGO_SSL})
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((error) => console.error('Could not connect to MongoDB', error));
+// // Route de Product
+// app.use('/', productRoutes);
 
+
+// const PORT = process.env.PORT || 8082;
+// app.listen(8082, () => {
+//   console.log(`ProductService Server is running on port ${PORT} `);
+// });
+
+
+require("dotenv").config();
+const app = require("./app.js");
 
 const PORT = process.env.PORT || 8082;
-app.listen(8082, () => {
-  console.log(`ProductService Server is running on port ${PORT} `);
-});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the products microservice !")
+})
+
+app.listen(PORT, () => {
+    console.log(`Products microservice is listening on ${PORT}`)
+})
